15b6a29a42da460674552c6ce835d691e24969ed
wupei1994 <969631699@qq.com>
2018-01-18 10:46:51 +0800

    first commit

---
+++ src/main/java/com/bl/MailTask.java
@@ -0,0 +1,11 @@
+package com.bl;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MailTask {
+
+    public static void main(String[] agrs){
+
+    }
+
+}
